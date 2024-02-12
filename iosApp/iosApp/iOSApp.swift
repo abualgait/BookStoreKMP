cb71762fabc410d0b3a863f1c1b1c591b7970f0b
MuhammadSayed <mohamed.sayed@salesrabbit.com>
2024-02-12 19:26:30 +0200

    refactor: start koin from iOS app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,15 @@
-import SwiftUI
+ import SwiftUI
 
 @main
-struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+struct NotflixApp: App {
+
+    init(){
+        KoinApplication.start()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
 }
