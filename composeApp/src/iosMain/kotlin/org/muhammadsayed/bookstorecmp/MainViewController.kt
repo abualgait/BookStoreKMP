8f941993fd1fae7078fa1d71453a4378f0bb0bf4
Abdul Basit <abdulbasit5361234@gmail.com>
2024-03-06 20:51:53 +0500

    Add KoinStart to initial Koin modules

--- composeApp/src/iosMain/kotlin/org/muhammadsayed/bookstorecmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/muhammadsayed/bookstorecmp/MainViewController.kt
@@ -1,6 +1,7 @@
 package org.muhammadsayed.bookstorecmp
 
 import androidx.compose.ui.window.ComposeUIViewController
-import org.muhammadsayed.bookstorecmp.App
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App()
+}
